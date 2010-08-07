383cab7214907d70c1e70b4ee98cabbf11d041b5
JaWi <j.w.janssen@lxtreme.nl>
2010-08-07 12:20:16 +0200

    Version bumps; small update on the run script for Windows hosts.

--- ols.distribution/run.bat
+++ ols.distribution/run.bat
@@ -1,3 +1,18 @@
 @echo off
 
-%JAVA_HOME%/bin/java -Dnl.lxtreme.ols.bundle.dir=plugins/ -cp "bin/*" nl.lxtreme.ols.runner.Runner
+rem Check whether the "magic" Java binary is available...
+java --version > NUL 2> NUL
+if errorlevel 1 goto noJVM
+
+rem For now, use the "console enabled" java for Windows...
+java -Dnl.lxtreme.ols.bundle.dir=plugins/ -cp "bin/*" nl.lxtreme.ols.runner.Runner
+goto end
+
+:noJVM
+echo It appears that Java is not installed on this computer. You
+echo should download and install the latest JDK.
+prompt
+exit 1
+
+:end
+exit 0
