b59ac5fbcf7b81218258bffb147eb6b50e68a13a
J.W. Janssen <j.w.janssen@lxtreme.nl>
2010-08-20 16:26:03 +0200

    Make run script independent of current working directory (Thanks Wayoda).

--- ols.distribution/run.sh
+++ ols.distribution/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# Ensure current working dir equals the dir of this script (thanks Wayoda)
+cd "$(dirname -- "${0}")"
 java -Dnl.lxtreme.ols.bundle.dir=plugins/ -cp "bin/*" nl.lxtreme.ols.runner.Runner
