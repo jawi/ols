dabfa62e3fb5bc54ff71745bff7c7039bb2bf233
JaWi <j.w.janssen@lxtreme.nl>
2010-12-19 17:52:14 +0100

    Made the disk image for OSX include a shortcut to Applications, for easier installing.

---
+++ ols.distribution/app-bundle-osx.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+INPUT_DMG=$1
+VOLUME_TITLE=$2
+
+device=$(hdiutil attach -readwrite -noverify -owners off -noautoopen "${INPUT_DMG}" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+
+echo '
+   tell application "Finder"
+     tell disk "'${VOLUME_TITLE}'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 885, 430}
+           set theViewOptions to the icon view options of container window
+           set arrangement of theViewOptions to not arranged
+           set icon size of theViewOptions to 72
+           set background picture of theViewOptions to file ".background:'background.png'"
+           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+           set position of item "'${VOLUME_TITLE}'" of container window to {100, 150}
+           set position of item "Applications" of container window to {375, 150}
+           close
+           open
+           update without registering applications
+     end tell
+   end tell
+' | osascript
+
+chmod -Rf go-w /Volumes/${VOLUME_TITLE}/*
+sync
+sync
+hdiutil detach ${device}
+
+###EOF###
