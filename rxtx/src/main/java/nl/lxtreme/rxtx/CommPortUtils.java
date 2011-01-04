35211a85f9fa5fcf58ba0f3e4b66d26b74452576
J.W. Janssen <j.w.janssen@lxtreme.nl>
2011-01-04 16:42:12 +0100

    Minor cleanups.

--- rxtx/src/main/java/nl/lxtreme/rxtx/CommPortUtils.java
+++ rxtx/src/main/java/nl/lxtreme/rxtx/CommPortUtils.java
@@ -24,7 +24,6 @@ package nl.lxtreme.rxtx;
 import gnu.io.*;
 
 import java.io.*;
-import java.util.regex.*;
 
 
 /**
@@ -33,11 +32,6 @@ import java.util.regex.*;
  */
 public final class CommPortUtils
 {
-  // VARIABLES
-
-  private static boolean initialized = false;
-  private static String osName = "";
-
   // CONSTRUCTORS
 
   /**
@@ -70,147 +64,25 @@ public final class CommPortUtils
     {
       throw new IllegalArgumentException( "Port name cannot be null!" );
     }
-
-    if ( !initialized )
-    {
-      initialize();
-      System.out.println( RXTXVersion.getVersion() + " initialized..." );
-      initialized = true;
-    }
-
-    RXTXPort port = null;
-    int tries = 3;
-
     if ( !specialFileExists( aPortName ) )
     {
       throw new NoSuchPortException();
     }
 
-    do
-    {
-      try
-      {
-        port = internalGetSerialPort( aPortName );
-      }
-      catch ( NoSuchPortException exception )
-      {
-        addSerialPort( aPortName );
-      }
-    }
-    while ( ( port == null ) && ( tries >= 0 ) );
-
-    if ( port == null )
-    {
-      throw new NoSuchPortException();
-    }
-
+    final RXTXPort port = new RXTXPort( aPortName );
     return port;
   }
 
   /**
-   * Tries to verify the given port name as a valid device and if this appears
-   * so, adds it to the list of devices of RXTX.
+   * Checks whether the path (to a presumed port) exists or not.
+   * <p>
+   * This method is needed to avoid problems on platforms like Windows where
+   * trying to open non-existing ports causes JVM-errors.
+   * </p>
    * 
-   * @param aPortName
-   *          the name of the port to add, cannot be <code>null</code>.
-   */
-  private static void addSerialPort( final String aPortName ) throws NoSuchPortException
-  {
-    if ( !osName.contains( "windows" ) )
-    {
-      File device = new File( aPortName );
-      try
-      {
-        device = device.getCanonicalFile();
-        if ( !device.exists() )
-        {
-          throw new NoSuchPortException();
-        }
-      }
-      catch ( IOException exception )
-      {
-        throw new NoSuchPortException();
-      }
-    }
-    else
-    {
-      final Pattern pattern = Pattern.compile( "^COM\\d+$", Pattern.CASE_INSENSITIVE );
-      final Matcher matcher = pattern.matcher( aPortName );
-      if ( !matcher.matches() )
-      {
-        throw new NoSuchPortException();
-      }
-    }
-
-    // Try to add the portname as possible serial port...
-    CommPortIdentifier.addPortName( aPortName, CommPortIdentifier.PORT_SERIAL, createDriver() );
-  }
-
-  /**
-   * Creates a new CommDriver instance.
-   * 
-   * @return a CommDriver instance, fully initialized, never <code>null</code>.
-   */
-  private static CommDriver createDriver()
-  {
-    final RXTXCommDriver driver = new RXTXCommDriver();
-    driver.initialize();
-    return driver;
-  }
-
-  /**
-   * Initializes this RXTX library with some platform-specific hacks.
-   */
-  private static void initialize()
-  {
-    // Suppress the version mismatch banner...
-    System.setProperty( "gnu.io.rxtx.NoVersionOutput", "true" );
-
-    osName = System.getProperty( "os.name" );
-    if ( osName == null )
-    {
-      osName = "unknown";
-    }
-    osName = osName.toLowerCase();
-
-    // Ugly hack: force a greater set of port names for some OSs...
-    if ( osName.contains( "linux" ) )
-    {
-      // Set taken from RXTXCommDriver.java ...
-      final String[] portPrefixes = { "/dev/ttyS", "/dev/ttySA", "/dev/ttyACM", "/dev/ttyUSB", "/dev/rfcomm",
-          "/dev/ttyircomm" };
-      final StringBuilder sb = new StringBuilder();
-      for ( String portPrefix : portPrefixes )
-      {
-        for ( int i = 0; i < 8; i++ )
-        {
-          if ( sb.length() > 0 )
-          {
-            sb.append( ":" );
-          }
-          sb.append( portPrefix + i );
-        }
-      }
-      System.setProperty( "gnu.io.rxtx.SerialPorts", sb.toString() );
-    }
-  }
-
-  /**
-   * @param aPortName
-   * @return
-   * @throws NoSuchPortException
-   * @throws PortInUseException
-   */
-  private static RXTXPort internalGetSerialPort( final String aPortName ) throws NoSuchPortException,
-      PortInUseException
-  {
-    final CommPortIdentifier portId = CommPortIdentifier.getPortIdentifier( aPortName );
-    return ( RXTXPort )portId.open( "RxTx client library", 1000 );
-  }
-
-  /**
    * @param aPath
-   * @return
+   *          the path to the presumed port, cannot be <code>null</code>.
+   * @return <code>true</code> if the port exists, <code>false</code> otherwise.
    */
   private static boolean specialFileExists( final String aPath )
   {
