8773ac93b96a216cf8a7b9666947bb4449c5f43b
JaWi <j.w.janssen@lxtreme.nl>
2011-11-18 18:46:01 +0100

    Fixed some small bugs found by testing on other platforms:
    - the L&F was not always correctly set due to the order
      in which bundles are started. This could lead to half-
      applied L&F;
    - the application icon on OSX wasn't set correctly;
    - the run batch file for Windows didn't work correctly
      with latest JRE.

--- util/src/main/java/nl/lxtreme/ols/util/internal/Activator.java
+++ util/src/main/java/nl/lxtreme/ols/util/internal/Activator.java
@@ -21,10 +21,7 @@
 package nl.lxtreme.ols.util.internal;
 
 
-import java.awt.event.*;
 import java.util.logging.*;
-
-import javax.swing.*;
 
 import nl.lxtreme.ols.util.*;
 import nl.lxtreme.ols.util.swing.*;
@@ -39,42 +36,6 @@ import org.osgi.framework.*;
  */
 public class Activator extends DependencyActivatorBase
 {
-  // INNER TYPES
-
-  /**
-   * Provides an {@link Action} for closing a {@link JOptionPane}.
-   */
-  static final class CloseOptionPaneAction extends AbstractAction
-  {
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
-    @Override
-    public void actionPerformed( final ActionEvent aEvent )
-    {
-      final JOptionPane optionPane = ( JOptionPane )aEvent.getSource();
-      optionPane.setValue( Integer.valueOf( JOptionPane.CLOSED_OPTION ) );
-    }
-  }
-
-  /**
-   * Provides a hack to ensure the system class loader is used at all times when
-   * loading UI classes/resources/...
-   */
-  static class CLValue implements UIDefaults.ActiveValue
-  {
-    /**
-     * @see javax.swing.UIDefaults.ActiveValue#createValue(javax.swing.UIDefaults)
-     */
-    public @Override
-    ClassLoader createValue( final UIDefaults aDefaults )
-    {
-      return Activator.class.getClassLoader();
-    }
-  }
-
   // METHODS
 
   /**
@@ -93,8 +54,6 @@ public class Activator extends DependencyActivatorBase
   public void init( final BundleContext aContext, final DependencyManager aManager ) throws Exception
   {
     final HostProperties hostProperties = new HostResourceProperties( aContext );
-
-    initOSSpecifics( hostProperties.getShortName() );
 
     logEnvironment( hostProperties );
 
@@ -124,68 +83,6 @@ public class Activator extends DependencyActivatorBase
   }
 
   /**
-   * Initializes the OS-specific stuff.
-   * 
-   * @param aApplicationName
-   *          the name of the application (when this needs to be passed to the
-   *          guest OS);
-   * @param aApplicationCallback
-   *          the application callback used to report application events on some
-   *          platforms (Mac OS), may be <code>null</code>.
-   */
-  private void initOSSpecifics( final String aApplicationName )
-  {
-    final HostInfo hostInfo = HostUtils.getHostInfo();
-    if ( hostInfo.isMacOS() )
-    {
-      // Moves the main menu bar to the screen menu bar location...
-      System.setProperty( "apple.laf.useScreenMenuBar", "true" );
-      System.setProperty( "apple.awt.graphics.EnableQ2DX", "true" );
-      System.setProperty( "com.apple.mrj.application.apple.menu.about.name", aApplicationName );
-      System.setProperty( "com.apple.mrj.application.growbox.intrudes", "false" );
-      System.setProperty( "com.apple.mrj.application.live-resize", "false" );
-      System.setProperty( "com.apple.macos.smallTabs", "true" );
-      System.setProperty( "apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS" );
-
-      // Install an additional accelerator (Cmd+W) for closing option panes...
-      ActionMap map = ( ActionMap )UIManager.get( "OptionPane.actionMap" );
-      if ( map == null )
-      {
-        map = new ActionMap();
-        UIManager.put( "OptionPane.actionMap", map );
-      }
-      map.put( "close", new CloseOptionPaneAction() );
-
-      UIManager.put( "OptionPane.windowBindings", //
-          new Object[] { SwingComponentUtils.createMenuKeyMask( KeyEvent.VK_W ), "close", "ESCAPE", "close" } );
-    }
-    else if ( hostInfo.isUnix() )
-    {
-      try
-      {
-        UIManager.put( "Application.useSystemFontSettings", Boolean.FALSE );
-        setLookAndFeel( "com.jgoodies.looks.plastic.Plastic3DLookAndFeel" );
-      }
-      catch ( Exception exception )
-      {
-        Logger.getAnonymousLogger().log( Level.WARNING, "Failed to set look and feel!", exception );
-      }
-    }
-    else if ( hostInfo.isWindows() )
-    {
-      try
-      {
-        UIManager.put( "Application.useSystemFontSettings", Boolean.TRUE );
-        setLookAndFeel( "com.jgoodies.looks.plastic.PlasticXPLookAndFeel" );
-      }
-      catch ( Exception exception )
-      {
-        Logger.getAnonymousLogger().log( Level.WARNING, "Failed to set look and feel!", exception );
-      }
-    }
-  }
-
-  /**
    * @param aContext
    */
   private void logEnvironment( final HostProperties aProperties )
@@ -203,32 +100,4 @@ public class Activator extends DependencyActivatorBase
     Logger.getLogger( getClass().getName() ).info( sb.toString() );
   }
 
-  /**
-   * @param aLookAndFeelClass
-   */
-  private void setLookAndFeel( final String aLookAndFeelClassName )
-  {
-    final UIDefaults defaults = UIManager.getDefaults();
-    // to make sure we always use system class loader
-    defaults.put( "ClassLoader", new CLValue() );
-
-    final ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
-    try
-    {
-      Thread.currentThread().setContextClassLoader( Activator.class.getClassLoader() );
-      UIManager.setLookAndFeel( aLookAndFeelClassName );
-    }
-    catch ( Exception exception )
-    {
-      // Make sure to handle IO-interrupted exceptions properly!
-      if ( !HostUtils.handleInterruptedException( exception ) )
-      {
-        Logger.getAnonymousLogger().log( Level.WARNING, "Failed to set look and feel!", exception );
-      }
-    }
-    finally
-    {
-      Thread.currentThread().setContextClassLoader( oldCL );
-    }
-  }
 }
