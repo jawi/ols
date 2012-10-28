4f2772ba805cab627f1b42055e956ba8277e59f3
Jan Willem Janssen <janwillem.janssen@luminis.eu>
2012-10-28 14:38:32 +0100

    Removed old Maven projects.

--- ols.distribution/src/main/resources/install-to-maven.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mvn install:install-file -DgroupId=nl.lxtreme.ols -DartifactId=api -Dversion=1.0.4 -Dpackaging=jar -Dfile=lib/api-1.0.4.jar -Djavadoc=javadoc/api-1.0.4-javadoc.jar
-mvn install:install-file -DgroupId=nl.lxtreme.ols -DartifactId=util -Dversion=1.0.5 -Dpackaging=jar -Dfile=lib/util-1.0.5.jar -Djavadoc=javadoc/util-1.0.5-javadoc.jar
-mvn install:install-file -DgroupId=nl.lxtreme.ols -DartifactId=base -Dversion=1.0.0 -Dpackaging=jar -Dfile=lib/base-1.0.0.jar -Djavadoc=javadoc/base-1.0.0-javadoc.jar
-mvn install:install-file -DgroupId=nl.lxtreme.ols -DartifactId=test.util -Dversion=1.0.0 -Dpackaging=jar -Dfile=lib/test.util-1.0.0.jar -Djavadoc=javadoc/test.util-1.0.0-javadoc.jar
--- service.io/src/main/java/javax/microedition/io/StreamConnection.java
+++
@@ -1,9 +0,0 @@
-package javax.microedition.io;
-
-
-/**
- * @since CLDC 1.0
- */
-public interface StreamConnection extends InputConnection, OutputConnection
-{
-}
