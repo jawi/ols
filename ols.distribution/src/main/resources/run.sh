4ba27d0cdb58dbc65e45e22397a71bd327a73b51
Jan Willem Janssen <janwillem.janssen@luminis.eu>
2013-01-26 16:57:31 +0100

    Issue #143: fixes for the Un*x run-scripts. Thanks to R.Diez for mentioning this.

--- ols.distribution/src/main/resources/run.sh
+++ ols.distribution/src/main/resources/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rem Simple check to see whether the "magic" Java binary is available on our path;
+# Simple check to see whether the "magic" Java binary is available on our path;
 java -version 1>/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
   echo It appears that Java is not installed on this computer. You
@@ -13,9 +13,9 @@ BASEDIR=$(dirname -- "${0}")
 # all paths are used relatively from the base dir...
 PLUGINDIR=$BASEDIR/plugins
 CLASSPATH=$BASEDIR/bin/*
-rem give the client roughly 1gigabyte of memory 
+# give the client roughly 1gigabyte of memory 
 MEMSETTINGS=-Xmx1024m
-rem <https://github.com/jawi/ols/issues/125>
-SYSPROPS=-Djna.nosys=true -Dnl.lxtreme.ols.bundle.dir="$PLUGINDIR" -DPlastic.defaultTheme=SkyBluer
+# see: <https://github.com/jawi/ols/issues/125> & <https://github.com/jawi/ols/issues/143>
+SYSPROPS="-Djna.nosys=true -Dnl.lxtreme.ols.bundle.dir=\"$PLUGINDIR\" -DPlastic.defaultTheme=SkyBluer"
 
-java $MEMSETTINGS $SYSPROPS -cp "$CLASSPATH" nl.lxtreme.ols.runner.Runner
+java "$MEMSETTINGS" "$SYSPROPS" -cp "$CLASSPATH" nl.lxtreme.ols.runner.Runner
