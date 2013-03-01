5e5b8b6268e7121d079c4fc7f92c4b724b7e68dc
Jan Willem Janssen <janwillem.janssen@luminis.eu>
2013-03-01 23:58:23 +0100

    Issue #140: yet another fix for this:
    - the path ended up being set incorrectly. Thanks to MarkusG for pointing this out.

--- ols.distribution/src/main/resources/run.bat
+++ ols.distribution/src/main/resources/run.bat
@@ -8,7 +8,7 @@ goto setup
 
 :findJavaAdvanced
 rem See <https://github.com/jawi/ols/issues/140>
-for /F "tokens=2*" %%A in ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\TypeLib\{5852F5E0-8BF4-11D4-A245-0080C6F74284}\1.0\HELPDIR" /ve') do set JavaPath="%%B\javaw.exe"
+for /F "tokens=2*" %%A in ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\TypeLib\{5852F5E0-8BF4-11D4-A245-0080C6F74284}\1.0\HELPDIR" /ve') do set JavaPath="%%B"
 set path=%path%;%JavaPath%
 java -version > NUL 2> NUL
 if errorlevel 1 goto noJVM
