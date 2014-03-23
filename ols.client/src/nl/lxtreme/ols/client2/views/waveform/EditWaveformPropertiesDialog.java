jawi/ols

commit 902ebf3ada1a419a8f19c1d76ec3ccba86e26bba

Bugfixes and improvements:
- added dialogs for editing cursor & element properties;
- fixed an issue where the height waveform view was not correctly
  tracking the viewport height.

ols.client/src/nl/lxtreme/ols/client2/views/waveform/EditWaveformPropertiesDialog.java
@@ -1,0 +1,373 @@
+/*
+ * OpenBench LogicSniffer / SUMP project 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ * Copyright (C) 2006-2010 Michael Poppitz, www.sump.org
+ * Copyright (C) 2010 J.W. Janssen, www.lxtreme.nl
+ */
+package nl.lxtreme.ols.client2.views.waveform;
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import nl.lxtreme.ols.client2.views.*;
+import nl.lxtreme.ols.client2.views.UIMgr.Alignment;
+import nl.lxtreme.ols.client2.views.waveform.WaveformElement.Type;
+import nl.lxtreme.ols.util.swing.*;
+import nl.lxtreme.ols.util.swing.StandardActionFactory.DialogStatus;
+import nl.lxtreme.ols.util.swing.StandardActionFactory.StatusAwareCloseableDialog;
+import nl.lxtreme.ols.util.swing.component.*;
+import nl.lxtreme.ols.util.swing.validation.*;
+
+
+/**
+ * Provides a Swing dialog for editing a signal element's label and color.
+ */
+public class EditWaveformPropertiesDialog extends JDialog implements StatusAwareCloseableDialog
+{
+  // INNER TYPES
+
+  /**
+   * Provides a signal alignment combobox renderer.
+   */
+  static final class AlignmentComboBoxRenderer extends EnumItemRenderer<Alignment>
+  {
+    // CONSTANTS
+
+    private static final long serialVersionUID = 1L;
+
+    // METHODS
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected String getDisplayValue( final Alignment aValue )
+    {
+      switch ( aValue )
+      {
+        case BOTTOM:
+          return "Bottom";
+        case TOP:
+          return "Top";
+        case CENTER:
+          return "Center";
+        default:
+      }
+      return super.getDisplayValue( aValue );
+    }
+  }
+
+  // CONSTANTS
+
+  private static final long serialVersionUID = 1L;
+
+  // VARIABLES
+
+  private final String defaultLabel;
+  private final Color defaultColor;
+  private final int defaultHeight;
+  private final int defaultSignalHeight;
+  private final Alignment defaultAlignment;
+
+  private JTextField labelEditor;
+  private JColorEditor colorEditor;
+  private JTextField heightEditor;
+  private JTextField signalHeightEditor;
+  private JComboBox signalAlignmentEditor;
+
+  boolean dialogResult = false;
+
+  // CONSTRUCTORS
+
+  /**
+   * Creates a new {@link EditWaveformPropertiesDialog} instance.
+   * 
+   * @param aWindow
+   *          the parent window for this dialog, can be <code>null</code>;
+   * @param aElement
+   *          the UI-element to edit the label for, cannot be <code>null</code>.
+   */
+  public EditWaveformPropertiesDialog( final Window aParent, final WaveformElement aElement )
+  {
+    super( aParent, ModalityType.APPLICATION_MODAL );
+
+    this.defaultLabel = aElement.getLabel();
+    this.defaultColor = aElement.getColor();
+    this.defaultHeight = aElement.getHeight();
+
+    this.defaultSignalHeight = aElement.getSignalHeight();
+    this.defaultAlignment = aElement.getAlignment();
+
+    initDialog( aElement );
+  }
+
+  /**
+   * Determines the title for this dialog based on the given signal element.
+   * 
+   * @param aElement
+   *          the UI-element to determine the title for, cannot be
+   *          <code>null</code>.
+   * @return a title, never <code>null</code>.
+   */
+  private static String getTitle( final WaveformElement aElement )
+  {
+    String name = "";
+    switch ( aElement.getType() )
+    {
+      case ANALOG_SCOPE:
+        name = "scope";
+        break;
+      case CHANNEL:
+        name = "channel";
+        break;
+      case GROUP:
+        name = "signal group";
+        break;
+      case GROUP_SUMMARY:
+        name = "group summary";
+        break;
+    }
+    return "Edit " + name + " properties";
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void close()
+  {
+    setVisible( false );
+    dispose();
+  }
+
+  /**
+   * @return
+   */
+  public Color getColor()
+  {
+    return this.colorEditor.getColor();
+  }
+
+  /**
+   * Returns the new element height.
+   * 
+   * @return the new element height, in pixels.
+   */
+  public int getElementHeight()
+  {
+    try
+    {
+      return Integer.parseInt( this.heightEditor.getText() );
+    }
+    catch ( NumberFormatException exception )
+    {
+      return UIManager.getInt( UIMgr.CHANNEL_HEIGHT );
+    }
+  }
+
+  /**
+   * Returns the new label.
+   * 
+   * @return the new label, can be <code>null</code>.
+   */
+  public String getLabel()
+  {
+    return this.labelEditor.getText();
+  }
+
+  /**
+   * Returns the new signal height.
+   * 
+   * @return the new signal height, in pixels.
+   */
+  public Alignment getAlignment()
+  {
+    Alignment result = ( Alignment )this.signalAlignmentEditor.getSelectedItem();
+    if ( result == null )
+    {
+      result = Alignment.valueOf( UIManager.getString( UIMgr.SIGNALVIEW_SIGNAL_ALIGNMENT ) );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the new signal height.
+   * 
+   * @return the new signal height, in pixels.
+   */
+  public int getSignalHeight()
+  {
+    try
+    {
+      return Integer.parseInt( this.signalHeightEditor.getText() );
+    }
+    catch ( NumberFormatException exception )
+    {
+      return UIManager.getInt( UIMgr.DIGITAL_SIGNAL_HEIGHT );
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean setDialogStatus( final DialogStatus aStatus )
+  {
+    boolean isOkButton = ( aStatus == DialogStatus.OK );
+    String text = this.labelEditor.getText();
+
+    if ( isOkButton && ( ( text == null ) || "".equals( text ) ) )
+    {
+      JOptionPane.showMessageDialog( getParent(), "No label defined!", //
+          "Invalid properties", JOptionPane.ERROR_MESSAGE );
+      return false;
+    }
+
+    this.dialogResult = isOkButton;
+    return true;
+  }
+
+  /**
+   * Makes this dialog visible on screen and waits until it is dismissed.
+   * 
+   * @return <code>true</code> if the dialog is acknowledged by the user,
+   *         <code>false</code> if it is cancelled by the user.
+   */
+  public boolean showDialog()
+  {
+    this.dialogResult = false;
+    setVisible( true );
+    return this.dialogResult;
+  }
+
+  /**
+   * Applies the default properties to the properties.
+   */
+  protected void applyDefaultProperties()
+  {
+    this.labelEditor.setText( this.defaultLabel );
+    this.colorEditor.setColor( this.defaultColor );
+    this.heightEditor.setText( "" + this.defaultHeight );
+    this.signalHeightEditor.setText( "" + this.defaultSignalHeight );
+    this.signalAlignmentEditor.setSelectedItem( this.defaultAlignment );
+  }
+
+  /**
+   * Initializes this dialog.
+   */
+  private void initDialog( final WaveformElement aElement )
+  {
+    setTitle( getTitle( aElement ) );
+    setLocationRelativeTo( getParent() );
+    setResizable( false );
+
+    final JButton okButton = StandardActionFactory.createOkButton();
+    JButton cancelButton = StandardActionFactory.createCancelButton();
+
+    JLabel labelEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Label" );
+    this.labelEditor = new JTextField( aElement.getLabel(), 10 );
+    this.labelEditor.getDocument().addDocumentListener( new DocumentListener()
+    {
+      @Override
+      public void changedUpdate( final DocumentEvent aEvent )
+      {
+        updateOkButton( aEvent );
+      }
+
+      @Override
+      public void insertUpdate( final DocumentEvent aEvent )
+      {
+        updateOkButton( aEvent );
+      }
+
+      @Override
+      public void removeUpdate( final DocumentEvent aEvent )
+      {
+        updateOkButton( aEvent );
+      }
+
+      private void updateOkButton( final DocumentEvent aEvent )
+      {
+        String text = EditWaveformPropertiesDialog.this.labelEditor.getText();
+        okButton.setEnabled( ( text != null ) && !"".equals( text.trim() ) );
+      }
+    } );
+
+    JLabel colorEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Color" );
+    this.colorEditor = new JColorEditor( aElement.getColor() );
+
+    JLabel heightEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Height" );
+    this.heightEditor = new JTextField( "" + aElement.getHeight(), 10 );
+    this.heightEditor.setInputVerifier( JComponentInputVerifier.create( Integer.class,
+        "Invalid height! Must be a postive whole number." ) );
+
+    JLabel signalHeightEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Signal height" );
+    this.signalHeightEditor = new JTextField( "" + this.defaultSignalHeight, 10 );
+    this.signalHeightEditor.setInputVerifier( JComponentInputVerifier.create( Integer.class,
+        "Invalid height! Must be a postive whole number." ) );
+
+    JLabel signalAlignmentEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Signal alignment" );
+    this.signalAlignmentEditor = new JComboBox( Alignment.values() );
+    this.signalAlignmentEditor.setRenderer( new AlignmentComboBoxRenderer() );
+    this.signalAlignmentEditor.setSelectedItem( this.defaultAlignment );
+
+    JButton resetButton = new JButton( "Reset to defaults" );
+    resetButton.addActionListener( new ActionListener()
+    {
+      @Override
+      public void actionPerformed( final ActionEvent aEvent )
+      {
+        applyDefaultProperties();
+      }
+    } );
+
+    JPanel resetButtonPanel = new JPanel();
+    resetButtonPanel.add( resetButton, BorderLayout.LINE_END );
+
+    JPanel editorPane = new JPanel( new SpringLayout() );
+    editorPane.add( labelEditorLabel );
+    editorPane.add( this.labelEditor );
+
+    editorPane.add( colorEditorLabel );
+    editorPane.add( this.colorEditor );
+
+    editorPane.add( heightEditorLabel );
+    editorPane.add( this.heightEditor );
+
+    if ( Type.CHANNEL.equals( aElement.getType() ) )
+    {
+      editorPane.add( signalHeightEditorLabel );
+      editorPane.add( this.signalHeightEditor );
+
+      editorPane.add( signalAlignmentEditorLabel );
+      editorPane.add( this.signalAlignmentEditor );
+    }
+
+    editorPane.add( new JLabel( "" ) );
+    editorPane.add( resetButtonPanel );
+
+    SpringLayoutUtils.makeEditorGrid( editorPane, 10, 10 );
+
+    JComponent buttonPane = SwingComponentUtils.createButtonPane( okButton, cancelButton );
+
+    SwingComponentUtils.setupWindowContentPane( this, editorPane, buttonPane, okButton );
+  }
+}

ols.client/resources/ols.ui.defaults.cfg
@@ -1,5 +1,7 @@
 # Generic
 ols.current.color.scheme.enum = Default
+# [r=86,g=174,b=154]
+ols.cursor.default.color = 56ae9a
 ols.mousewheel.zoom.default.boolean = false
 ols.snap.cursors.default.boolean = false
 ols.show.tool.windows.default.boolean = false

ols.client/src/nl/lxtreme/ols/client2/usersettings/UserSettingsProviderImpl.java
@@ -347,8 +347,17 @@ public class UserSettingsProviderImpl implements UserSettingProvider
       {
         return false;
       }
-      return ( ( aComponent instanceof JFrame ) && !( ( JFrame )aComponent ).isUndecorated() )
-          || ( ( aComponent instanceof JDialog ) && !( ( JDialog )aComponent ).isUndecorated() );
+      if ( aComponent instanceof JFrame )
+      {
+        JFrame frame = ( JFrame )aComponent;
+        return !frame.isUndecorated() && frame.isResizable();
+      }
+      if ( aComponent instanceof JDialog )
+      {
+        JDialog dialog = ( JDialog )aComponent;
+        return !dialog.isUndecorated() && dialog.isResizable();
+      }
+      return false;
     }
 
     /**

ols.client/src/nl/lxtreme/ols/client2/views/EditCursorDialog.java
@@ -1,0 +1,181 @@
+/*
+ * OpenBench LogicSniffer / SUMP project 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *
+ * Copyright (C) 2006-2010 Michael Poppitz, www.sump.org
+ * Copyright (C) 2010 J.W. Janssen, www.lxtreme.nl
+ */
+package nl.lxtreme.ols.client2.views;
+
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import nl.lxtreme.ols.common.acquisition.Cursor;
+import nl.lxtreme.ols.util.swing.*;
+import nl.lxtreme.ols.util.swing.StandardActionFactory.DialogStatus;
+import nl.lxtreme.ols.util.swing.StandardActionFactory.StatusAwareCloseableDialog;
+import nl.lxtreme.ols.util.swing.component.*;
+
+
+/**
+ * Provides a Swing dialog for editing a cursor label.
+ */
+public class EditCursorDialog extends JDialog implements StatusAwareCloseableDialog
+{
+  // CONSTANTS
+
+  private static final long serialVersionUID = 1L;
+
+  // VARIABLES
+
+  private final String defaultLabel;
+  private final Color defaultColor;
+
+  private JTextField labelEditor;
+  private JColorEditor colorEditor;
+
+  boolean dialogResult = false;
+
+  // CONSTRUCTORS
+
+  /**
+   * Creates a new EditCursorLabelAction.EditCursorDialog instance.
+   */
+  public EditCursorDialog( Window aParent, Cursor aCursor )
+  {
+    super( aParent, ModalityType.APPLICATION_MODAL );
+
+    this.defaultLabel = aCursor.getLabel();
+    this.defaultColor = aCursor.getColor();
+
+    initDialog( aCursor );
+  }
+
+  // METHODS
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void close()
+  {
+    setVisible( false );
+    dispose();
+  }
+
+  /**
+   * Returns the new cursor color.
+   * 
+   * @return the cursor color, can be <code>null</code>.
+   */
+  public Color getColor()
+  {
+    return this.colorEditor.getColor();
+  }
+
+  /**
+   * Returns the new cursor label.
+   * 
+   * @return the cursor label, can be <code>null</code>.
+   */
+  public String getLabel()
+  {
+    return this.labelEditor.getText();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean setDialogStatus( final DialogStatus aStatus )
+  {
+    this.dialogResult = ( aStatus == DialogStatus.OK );
+    return true;
+  }
+
+  /**
+   * Makes this dialog visible on screen and waits until it is dismissed.
+   * 
+   * @return <code>true</code> if the dialog is acknowledged by the user,
+   *         <code>false</code> if it is cancelled by the user.
+   */
+  public boolean showDialog()
+  {
+    this.dialogResult = false;
+    setVisible( true );
+    return this.dialogResult;
+  }
+
+  /**
+   * Applies the default properties to the properties.
+   */
+  protected void applyDefaultProperties()
+  {
+    this.labelEditor.setText( this.defaultLabel );
+    this.colorEditor.setColor( this.defaultColor );
+  }
+
+  /**
+   * Initializes this dialog.
+   */
+  private void initDialog( final Cursor aCursor )
+  {
+    setTitle( "Edit cursor properties" );
+    setLocationRelativeTo( getParent() );
+    setResizable( false );
+
+    JLabel labelEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Label" );
+    this.labelEditor = new JTextField( aCursor.getLabel(), 10 );
+
+    JLabel colorEditorLabel = SwingComponentUtils.createRightAlignedLabel( "Color" );
+    this.colorEditor = new JColorEditor( UIMgr.getCursorColor( aCursor ) );
+
+    JButton okButton = StandardActionFactory.createOkButton();
+    JButton cancelButton = StandardActionFactory.createCancelButton();
+
+    JButton resetButton = new JButton( "Reset to defaults" );
+    resetButton.addActionListener( new ActionListener()
+    {
+      @Override
+      public void actionPerformed( final ActionEvent aEvent )
+      {
+        applyDefaultProperties();
+      }
+    } );
+
+    JPanel resetButtonPanel = new JPanel();
+    resetButtonPanel.add( resetButton, BorderLayout.LINE_END );
+
+    JPanel editorPane = new JPanel( new SpringLayout() );
+    editorPane.add( labelEditorLabel );
+    editorPane.add( this.labelEditor );
+
+    editorPane.add( colorEditorLabel );
+    editorPane.add( this.colorEditor );
+
+    editorPane.add( new JLabel( "" ) );
+    editorPane.add( resetButtonPanel );
+    
+    SpringLayoutUtils.makeEditorGrid( editorPane, 10, 10 );
+
+    JComponent buttonPane = SwingComponentUtils.createButtonPane( okButton, cancelButton );
+    
+    SwingComponentUtils.setupWindowContentPane( this, editorPane, buttonPane, okButton );
+  }
+}

ols.client/src/nl/lxtreme/ols/client2/views/UIMgr.java
@@ -83,6 +83,9 @@ public final class UIMgr
   /** The default color for channels, group summary and the scope of group 4. */
   public static final String CHANNEL_GROUP4_DEFAULT_COLOR = "ols.channelgroup4.default.color";
 
+  /** The default color of cursors. */
+  public static final String CURSOR_DEFAULT_COLOR = "ols.cursor.default.color";
+
   /** The height of the signal group, in pixels. */
   public static final String SIGNAL_GROUP_HEIGHT = "ols.signalgroup.height";
   /** The height of the digital channels, in pixels. */
@@ -249,6 +252,10 @@ public final class UIMgr
 
       result = UIManager.getColor( key );
     }
+    if ( result == null )
+    {
+      result = UIManager.getColor( CURSOR_DEFAULT_COLOR );
+    }
     return ( result == null ) ? Color.ORANGE.darker() : result;
   }
 
@@ -263,7 +270,9 @@ public final class UIMgr
     Color result = aChannelGroup.getColor();
     if ( result == null )
     {
-      String key = String.format( "ols.channelgroup%d.default.color", aChannelGroup.getIndex() );
+      Integer groupIdx = Integer.valueOf( aChannelGroup.getIndex() + 1 );
+      
+      String key = String.format( "ols.channelgroup%d.default.color", groupIdx );
       result = UIManager.getColor( key );
     }
     return ( result == null ) ? Color.BLUE.darker() : result;

ols.client/src/nl/lxtreme/ols/client2/views/waveform/WaveformLabelComponent.java
@@ -64,6 +64,8 @@ class WaveformLabelComponent extends JComponent
     setInheritsPopupMenu( true );
     // Enable synthetic drag events (even when mouse is outside window)...
     setAutoscrolls( true );
+    // We can NOT receive the focus...
+    setFocusable( false );
   }
 
   // METHODS
@@ -82,20 +84,44 @@ class WaveformLabelComponent extends JComponent
   }
 
   /**
-   * {@inheritDoc}
+   * Determines the preferred width of this view, based on the current set of
+   * channel labels.
+   * 
+   * @return a width, in pixels.
    */
-  @Override
-  public void addNotify()
+  public int getPreferredWidth()
   {
-    super.addNotify();
+    int minWidth = 0;
 
-    // Bootstrap the width of this component...
-    int width = getPreferredWidth();
-    int height = this.model.calculateScreenHeight();
+    BufferedImage dummy = new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB );
+    Graphics2D canvas = dummy.createGraphics();
 
-    setPreferredSize( new Dimension( width, height ) );
+    int padding = ( 2 * getInt( CHANNELLABELS_PADDING, 1 ) ) + getInt( CHANNELLABELS_GUTTER_WIDTH, 1 );
 
-    revalidate();
+    try
+    {
+      final FontMetrics fm = canvas.getFontMetrics( UIMgr.getFont( CHANNELLABELS_LABEL_FONT ) );
+      for ( WaveformElement element : this.model.getWaveformElements() )
+      {
+        String label = element.getLabel();
+        if ( label == null )
+        {
+          label = "";
+        }
+        minWidth = Math.max( minWidth, fm.stringWidth( label ) + padding );
+      }
+    }
+    finally
+    {
+      canvas.dispose();
+      canvas = null;
+      dummy = null;
+    }
+
+    // And always ensure we've got at least a minimal width...
+    minWidth = Math.max( minWidth, getInt( CHANNELLABELS_MINIMAL_WIDTH, 50 ) );
+
+    return minWidth;
   }
 
   /**
@@ -177,47 +203,6 @@ class WaveformLabelComponent extends JComponent
 
     aCanvas.setColor( aColor );
     aCanvas.drawString( aText, aXpos, aYpos );
-  }
-
-  /**
-   * Determines the preferred width of this view, based on the current set of
-   * channel labels.
-   * 
-   * @return a width, in pixels.
-   */
-  private int getPreferredWidth()
-  {
-    int minWidth = 0;
-
-    BufferedImage dummy = new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB );
-    Graphics2D canvas = dummy.createGraphics();
-
-    int padding = ( 2 * getInt( CHANNELLABELS_PADDING, 1 ) ) + getInt( CHANNELLABELS_GUTTER_WIDTH, 1 );
-
-    try
-    {
-      final FontMetrics fm = canvas.getFontMetrics( UIMgr.getFont( CHANNELLABELS_LABEL_FONT ) );
-      for ( WaveformElement element : this.model.getWaveformElements() )
-      {
-        String label = element.getLabel();
-        if ( label == null )
-        {
-          label = "";
-        }
-        minWidth = Math.max( minWidth, fm.stringWidth( label ) + padding );
-      }
-    }
-    finally
-    {
-      canvas.dispose();
-      canvas = null;
-      dummy = null;
-    }
-
-    // And always ensure we've got at least a minimal width...
-    minWidth = Math.max( minWidth, getInt( CHANNELLABELS_MINIMAL_WIDTH, 50 ) );
-
-    return minWidth;
   }
 
   /**

ols.client/src/nl/lxtreme/ols/client2/views/waveform/WaveformModel.java
@@ -115,9 +115,6 @@ public class WaveformModel extends ViewModel
   /**
    * Returns the absolute height of the screen.
    * 
-   * @param aHeightProvider
-   *          the provider for the various element's heights, cannot be
-   *          <code>null</code>.
    * @return a screen height, in pixels, >= 0 && < {@value Integer#MAX_VALUE}.
    */
   public int calculateScreenHeight()
@@ -131,6 +128,16 @@ public class WaveformModel extends ViewModel
     }
 
     return height;
+  }
+
+  /**
+   * Returns the absolute width of the screen.
+   * 
+   * @return a screen width, in pixels, >= 0 && < {@value Integer#MAX_VALUE}.
+   */
+  public int calculateScreenWidth()
+  {
+    return ( int )( getZoomFactor() * getAbsoluteLength() );
   }
 
   /**

ols.client/src/nl/lxtreme/ols/client2/views/waveform/WaveformTimelineComponent.java
@@ -302,12 +302,14 @@ final class WaveformTimelineComponent extends JComponent
   public WaveformTimelineComponent( WaveformModel aModel )
   {
     this.model = aModel;
-    
+
     setOpaque( true );
     // Inherit the popup menu from our parent...
     setInheritsPopupMenu( true );
     // Enable synthetic drag events (even when mouse is outside window)...
     setAutoscrolls( true );
+    // We can NOT receive the focus...
+    setFocusable( false );
   }
 
   // METHODS
@@ -323,23 +325,6 @@ final class WaveformTimelineComponent extends JComponent
     hints.put( KEY_COLOR_RENDERING, VALUE_COLOR_RENDER_QUALITY );
     hints.put( KEY_RENDERING, VALUE_RENDER_QUALITY );
     return hints;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void addNotify()
-  {
-    super.addNotify();
-
-    // Bootstrap the width of this component...
-    int width = ( int )( this.model.getZoomFactor() * this.model.getAbsoluteLength() );
-    int height = getInt( TIMELINE_HEIGHT, 40 );
-
-    setPreferredSize( new Dimension( width, height ) );
-
-    revalidate();
   }
 
   /**

commit 1c62836e5fdb28b240dd948f1bc00db96bc8f388

fixed build failure (introduced in commit 902ebf3ada...) due to missing static qualifier for Type.CHANNEL enum import in .../waveform/EditWaveformPropertiesDialog.java

ols.client/src/nl/lxtreme/ols/client2/views/waveform/EditWaveformPropertiesDialog.java
@@ -29,7 +29,7 @@ import javax.swing.event.*;
 
 import nl.lxtreme.ols.client2.views.*;
 import nl.lxtreme.ols.client2.views.UIMgr.Alignment;
-import nl.lxtreme.ols.client2.views.waveform.WaveformElement.Type;
+import static nl.lxtreme.ols.client2.views.waveform.WaveformElement.Type.CHANNEL;
 import nl.lxtreme.ols.util.swing.*;
 import nl.lxtreme.ols.util.swing.StandardActionFactory.DialogStatus;
 import nl.lxtreme.ols.util.swing.StandardActionFactory.StatusAwareCloseableDialog;
@@ -410,7 +410,7 @@ public class EditWaveformPropertiesDialog extends JDialog implements StatusAware
     editorPane.add( heightEditorLabel );
     editorPane.add( this.heightEditor );
 
-    if ( Type.CHANNEL.equals( aElement.getType() ) )
+    if ( CHANNEL.equals( aElement.getType() ) )
     {
       editorPane.add( signalHeightEditorLabel );
       editorPane.add( this.signalHeightEditor );
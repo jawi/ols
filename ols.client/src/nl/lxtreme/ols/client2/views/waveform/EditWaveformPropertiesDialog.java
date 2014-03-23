1c62836e5fdb28b240dd948f1bc00db96bc8f388
Tudor <tudor.git@gmail.com>
2014-03-23 17:45:07 +0200

    fixed build failure (introduced in commit 902ebf3ada...) due to missing static qualifier for Type.CHANNEL enum import in .../waveform/EditWaveformPropertiesDialog.java

--- ols.client/src/nl/lxtreme/ols/client2/views/waveform/EditWaveformPropertiesDialog.java
+++ ols.client/src/nl/lxtreme/ols/client2/views/waveform/EditWaveformPropertiesDialog.java
@@ -29,7 +29,7 @@ import javax.swing.event.*;
 
 import nl.lxtreme.ols.client2.views.*;
 import nl.lxtreme.ols.client2.views.UIMgr.Alignment;
-import nl.lxtreme.ols.client2.views.waveform.WaveformElement.Type;
+import static nl.lxtreme.ols.client2.views.waveform.WaveformElement.Type.CHANNEL;
 import nl.lxtreme.ols.util.swing.*;
 import nl.lxtreme.ols.util.swing.StandardActionFactory.DialogStatus;
 import nl.lxtreme.ols.util.swing.StandardActionFactory.StatusAwareCloseableDialog;
@@ -410,7 +410,7 @@ public class EditWaveformPropertiesDialog extends JDialog implements StatusAware
     editorPane.add( heightEditorLabel );
     editorPane.add( this.heightEditor );
 
-    if ( Type.CHANNEL.equals( aElement.getType() ) )
+    if ( CHANNEL.equals( aElement.getType() ) )
     {
       editorPane.add( signalHeightEditorLabel );
       editorPane.add( this.signalHeightEditor );
